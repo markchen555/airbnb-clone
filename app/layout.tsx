f40baadc7aa79004f8a4de4f418a26cff99d4812
Mark Chen <markchen555@gmail.com>
2023-04-04 23:46:15 -0700

    added navbar with logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import { Nunito } from "next/font/google"
+import { Nunito } from "next/font/google";
 
-import './globals.css'
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
 
 const font = Nunito({
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
